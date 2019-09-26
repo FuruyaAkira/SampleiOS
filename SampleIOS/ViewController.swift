19478a05d5d8413056a030c4928129b0d24c52f8
akira furuya <a_furuya@waku-2.com>
2019-09-26 17:14:26 +0900

    singleページプロジェクト追加

---
+++ SampleIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleIOS
+//
+//  Created by UU119147 on 2019/09/26.
+//  Copyright © 2019 akira358. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
